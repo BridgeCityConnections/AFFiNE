748714caaa74d98506e78217303b025af1b82c7c
zzj3720 <zuozijian1994@gmail.com>
2025-03-21 15:52:49 +0800

    fix(editor): clear the search text after selecting a member

--- packages/frontend/core/src/blocksuite/database-block/properties/member/multi-member-select/index.tsx
+++ packages/frontend/core/src/blocksuite/database-block/properties/member/multi-member-select/index.tsx
@@ -76,6 +76,7 @@ class MemberManager {
       }
       this.ops.onChange([...this.selectedMembers.value, memberId]);
       this.moveSelectionAfterSelect(memberId);
+      this.ops.userListService.search('');
     } else {
       this.ops.onChange(memberId);
     }
